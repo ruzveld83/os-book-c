d1d27bfd7a224546b165aad75b4071a96c5e4e5b
Ruslan Izmaylov <ruslan-93@mail.ru>
2020-07-28 11:54:48 +0300

    ch3proj1: refactored parsing

--- chap3/proj1/main.c
+++ chap3/proj1/main.c
@@ -5,12 +5,14 @@
 #include <sys/wait.h>
 
 #define MAX_LINE 80 /* The maximum length of a command */
+#define MAX_ARGS MAX_LINE / 2 + 1
 
 int main(void) {
     char command[MAX_LINE] = "";
     char prev_command[MAX_LINE] = "";
-    char * args[MAX_LINE / 2 + 1]; /* command line arguments */
+    char * args[MAX_ARGS]; /* command line arguments */
     bool background = false;
+    bool invalid_command = true;
     int should_run = 1; /* flag to determine when to exit program */
     while (should_run) {
         printf("osh>");
@@ -18,34 +20,47 @@ int main(void) {
 
         fgets(command, MAX_LINE, stdin);
 
-        /* history */
         if (strcmp(command, "!!\n") == 0) {
             if (prev_command[0] == '\0') {
                 printf("No commands in history\n");
             }
             strcpy(command, prev_command);
+        } else if (strcmp(command, "exit\n") == 0) {
+            should_run = 0;
+            continue;
         } else if (strcmp(command, "") != 0 && strcmp(command, "\n") != 0) {
             strcpy(prev_command, command);
         }
 
         /* parsing a new command */
         {
+            int i = 0;
+            background = false;
+            invalid_command = false;
             char * tok = strtok(command, " \n");
-            char ** arg = args;
             while (tok != NULL) {
-                background = !strcmp(tok, "&");
-                *arg = tok;
-                ++arg;
+                if (background) {
+                    printf("No input is expected after '&'\n");
+                    invalid_command = true;
+                    break;
+                }
+                if (i >= MAX_ARGS - 1) {
+                    printf("Too many args\n");
+                    invalid_command = true;
+                    break;
+                }
+                if (strcmp(tok, "&") == 0) {
+                    background = true;
+                } else {
+                    args[i] = tok;
+                }
                 tok = strtok(NULL, " \n");
+                ++i;
             }
-            if (arg != args && strcmp(*(arg - 1), "&") == 0) {
-                *(arg - 1) = NULL;
-            } else {
-                *arg = NULL;
-            }
+            args[i] = NULL;
         }
 
-        if (args[0] == NULL) {
+        if (args[0] == NULL || invalid_command) {
             continue;
         }
 
